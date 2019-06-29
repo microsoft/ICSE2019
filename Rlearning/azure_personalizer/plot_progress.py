928f8b7b445ea4b8cf58c870e96d0d05cd851ed4
John Mark Agosta <johnmark.agosta@gmail.com>
2019-06-28 22:27:08 -0700

    add plot

--- Rlearning/azure_personalizer/plot_progress.py
+++ Rlearning/azure_personalizer/plot_progress.py
@@ -24,11 +24,15 @@ import numpy as np
 import pandas as pd
 
 from bokeh.plotting import figure, show
+#from bokeh.models.markers import Circle
+from bokeh.colors.groups import blue
+from bokeh.models import Label
 from bokeh.layouts import column
 
 
 ### config constants 
 VERBOSE = True
+PLOT = True
 plot_dir    = os.path.join(os.getcwd() , 'plots') #'ICSE2019/Rlearning/azure_personalizer/plots') ## Default for -o option
 
 data_list = ("pers_data2019-6-26-5-28-25.csv",
@@ -70,6 +74,26 @@ if __name__ == '__main__':
     index_offset = 0
     for k, a_file in enumerate(data_list):
         reward_df = read_one(a_file, k, reward_df)
-    print('Done!', '\n', reward_df.describe())
-    reward_df.to_csv('reward.csv', index=False)
+    # print('Done!', '\n', reward_df.describe())
+    if PLOT:
+        p = figure(plot_width = 1000, plot_height = 320, 
+        title = 'Learning curve',
+        x_axis_label = 'events', y_axis_label = 'reward')
+        grp_cnt = max(reward_df['group'])
+        for grp in range(grp_cnt):
+            segment = reward_df.loc[reward_df['group'] == grp]
+            nrows = segment.shape[0]
+            avg_reward = np.mean(segment['reward'].values)
+            p.circle(segment['count'], segment['reward'].values + np.random.random((nrows))-0.5, size=2, alpha= 0.8, color = blue[grp % 15])
+            p.line([min(segment['count'].values), max(segment['count'].values)], [avg_reward, avg_reward], color='black')
+            annote = Label(x = min(segment['count'].values), y =8, 
+                text= '{:.2f}'.format(avg_reward), 
+                background_fill_color='WhiteSmoke', 
+                text_font_size="8pt")
+            p.add_layout(annote)
+        show(p)
+        print(segment.describe())
+        print('Done!')
+    else:
+        reward_df.to_csv('reward.csv', index=False)
 #EOF
